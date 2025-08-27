20b07b447eb1ece9385310314af005595e58fac1
Prakash Pramanik <abhiabhi40989@gmail.com>
2025-08-27 18:00:46 +0530

    Added demo project as normal folder

---
+++ demoGradel/src/main/java/com/example/demoGradel/DemoGradelApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoGradel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGradelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGradelApplication.class, args);
+	}
+
+}
